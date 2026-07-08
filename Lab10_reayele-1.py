3748c4e98115a6faa847f87d49e4cb76743c890b
Rediet Ayele <reayele@gmail.com>
2026-07-07 22:32:24 -0400

    Working through Errors

--- Lab10_reayele-1.py
+++ Lab10_reayele-1.py
@@ -14,20 +14,48 @@ class WordAnalyzer:
         self.__filepath = pathlib.Path(filepath)
         self.__word_counts = {}
     
+    def process_file(self):
+        try:
+            if not self.__filepath.exists():
+                  raise FileNotFoundError
+            remove_punctuation = str.maketrans("", "", string.punctuation)
+            with self.__filepath.open("r", encoding="utf-8") as file:
+                  for line in file:
+                        line = line.lower()
+                        line = line.translate(remove_punctuation)
+                        words = line.split()
+                        
+                  for word in words:
+                    if word in self.__word_counts:
+                        self.__word_counts[word] += 1
+                    else:
+                        self.__word_counts[word] = 1
+            return True
+        except FileNotFoundError:
+              print("Error: File not found. ")
+              return False
+        
+def print_report(self):
+        words = list(self.__word_counts.keys())
+        words.sort()
+
+        for word in words:
+            print(word, "::", self.__word_counts[word])
+
 def main():
         files = {
-             "1": pathlib.Path("moby_dick_ch1.txt"),
-             "2": pathlib.Path("frankenstein_ch1.txt"),
-             "3": pathlib.Path("alice_ch1.txt"),
-             "4": pathlib.Path("pride_prejudice_ch1.txt")
+             "1": pathlib.Path("princess_mars.txt"),
+             "2": pathlib.Path("Tarzan.txt"),
+             "3": pathlib.Path("treasure_islands.txt"),
+             "4": pathlib.Path("monte_cristo.txt")
         }
         while True:
             print("\n --- Word Analyzer ---")
             print("Please select a file to analyze:")
-            print("1. Moby Dick")
-            print("2. Frankenstein")
-            print("3. Alice in Wonderland")
-            print("4. Pride and Prejudice")
+            print("1. Princess of Mars")
+            print("2. Tarzan")
+            print("3. Treasure Island")
+            print("4. Monte Cristo")
             print("5. Exit")
 
             choice = input("\nEnter your choice (1-5): ")
@@ -35,6 +63,9 @@ def main():
                 print("\nPrcoessing", files[choice], "...")
         
                 analyzer = WordAnalyzer(files[choice])
+                
+                if analyzer.process_file():
+                    analyzer.print_report()
                 
                 input("\nPress Enter to return to the menu...")
 
@@ -46,33 +77,8 @@ def main():
                 input("\nPress Enter to return to the menu...")
 if __name__ == "__main__":
                 main()
-def process_file(self):
-        try:
-            if not self.__filepath.exists():
-                  raise FileNotFoundError
-            remove_punctuation = str.maketrans("", "", string.punctuation)
-            with self.__filepath.open("r", encoding="utf-8") as file:
-                  for line in file:
-                        line = line.lower()
-                        line = line.transalte(remove_punctuation)
-                        words = line.split()
-                        for word in words: 
-                              if words in self.__word_counts:
-                                    self.__word_counts[word] += 1
-                              else:
-                                    self.__word_counts[word] = 1
-            return True
         
-        except FileNotFoundError: 
-              print("Error: File not found.")
-              return False
 
-def print_report(self):
-      words = list(self.__word_counts.keys())
-      words.sort()
-
-      for word in words: 
-            print(word, "::", self.__word_counts[word])
     
     
     
