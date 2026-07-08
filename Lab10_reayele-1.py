3138edbf6c0a5ff0166826c34e269a62321caed7
Rediet Ayele <reayele@gmail.com>
2026-07-07 21:47:07 -0400

    Added an alphabetical word count report

--- Lab10_reayele-1.py
+++ Lab10_reayele-1.py
@@ -13,64 +13,73 @@ class WordAnalyzer:
     def __init__(self, filepath):
         self.__filepath = pathlib.Path(filepath)
         self.__word_counts = {}
-    def main():
+    
+def main():
         files = {
-            "1": pathlib.Path("moby_dick_ch.txt"),
-            "2": pathlib.Path("frankenstein_ch1.txt"),
-            "3": pathlib.Path("alice_ch1.txt"),
-            "4": pathlib.Path("pride_prejudice_ch1.txt")
+             "1": pathlib.Path("moby_dick_ch1.txt"),
+             "2": pathlib.Path("frankenstein_ch1.txt"),
+             "3": pathlib.Path("alice_ch1.txt"),
+             "4": pathlib.Path("pride_prejudice_ch1.txt")
         }
         while True:
             print("\n --- Word Analyzer ---")
             print("Please select a file to analyze:")
             print("1. Moby Dick")
             print("2. Frankenstein")
-            print("3. ALice in Wonderland")
+            print("3. Alice in Wonderland")
             print("4. Pride and Prejudice")
             print("5. Exit")
 
             choice = input("\nEnter your choice (1-5): ")
             if choice in files: 
                 print("\nPrcoessing", files[choice], "...")
+        
+                analyzer = WordAnalyzer(files[choice])
+                
+                input("\nPress Enter to return to the menu...")
 
-            if choice == "5":
+            elif choice == "5":
                 print("\nGoodbye!")
                 break
-
             else:
                 print("\nInvalid choice. Please select from 1-5.")
                 input("\nPress Enter to return to the menu...")
-    if __name__ == "__main__":
+if __name__ == "__main__":
                 main()
-        
-    files = {
-         "1": pathlib.Path("moby_dic_ch1.txt"),
-         "2": pathlib.Path("moby_dic_ch1.txt"),
-         "3": pathlib.Path("moby_dic_ch1.txt"),
-         "4": pathlib.Path("moby_dic_ch1.txt")
-    }
-    
-    def process_file(self):
-        """Read the file and count each word."""
+def process_file(self):
         try:
             if not self.__filepath.exists():
-                raise FileNotFoundError
+                  raise FileNotFoundError
             remove_punctuation = str.maketrans("", "", string.punctuation)
             with self.__filepath.open("r", encoding="utf-8") as file:
-                for line in file:
-                    line = line.lower()
-                    line = line.translate(remove_punctuation)
-                    words = line.split()
-                    
-                    for word in words:
-                        if word in self.__word_counts:
-                            self.__word_counts[word] += 1
-                        else: 
-                            self.__word_counts[word] = 1
+                  for line in file:
+                        line = line.lower()
+                        line = line.transalte(remove_punctuation)
+                        words = line.split()
+                        for word in words: 
+                              if words in self.__word_counts:
+                                    self.__word_counts[word] += 1
+                              else:
+                                    self.__word_counts[word] = 1
             return True
-        except FileNotFoundError:
-            print("Error: File not found.")
-            return False
+        
+        except FileNotFoundError: 
+              print("Error: File not found.")
+              return False
 
+def print_report(self):
+      words = list(self.__word_counts.keys())
+      words.sort()
 
-            
+      for word in words: 
+            print(word, "::", self.__word_counts[word])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+           
